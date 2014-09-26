c84989f3303f4e88d7dc90d2e7d02e73184fce7e
Rob Mayoff <git@rob.dqd.com>
2014-09-26 14:13:38 -0500

    Initial Commit

---
+++ constraint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  constraint
+//
+//  Created by Rob Mayoff on 9/26/14.
+//  Copyright (c) 2014 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ constraint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  constraint
+//
+//  Created by Rob Mayoff on 9/26/14.
+//  Copyright (c) 2014 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
