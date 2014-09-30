e74ac1b9ea8aafd56142ef5c6316d949f1e2d9c3
Rob Mayoff <git@rob.dqd.com>
2014-09-30 12:00:29 -0500

    working demo

--- constraint/ViewController.m
+++ constraint/ViewController.m
@@ -7,21 +7,32 @@
 //
 
 #import "ViewController.h"
+@import CoreMedia;
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *constraint;
+@property (strong, nonatomic) IBOutlet UIView *container;
+
 @end
 
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+@implementation ViewController {
+    BOOL enableWorkaround : 1;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)panRecognizerDidFire:(UIPanGestureRecognizer *)panner {
+    CGFloat offset = self.constraint.constant + [panner translationInView:self.view].y;
+    offset = MAX(0, MIN(offset, self.view.bounds.size.height));
+    self.constraint.constant = offset;
+    [panner setTranslation:CGPointZero inView:self.view];
+
+    if (enableWorkaround) {
+        [self.container setNeedsLayout];
+    }
+}
+
+- (IBAction)viewWasTapped:(id)sender {
+    enableWorkaround = !enableWorkaround;
 }
 
 @end
